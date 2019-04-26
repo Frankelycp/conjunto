1953e5259cebc9f829410015794535622d0161a1
Frankelycp <50002863+Frankelycp@users.noreply.github.com>
2019-04-26 01:28:18 -0300

    Add files via upload

--- Conjunto.java
+++ Conjunto.java
@@ -1,10 +1,32 @@
 package conjunto;
 
-public class Conjunto {
+import java.util.Scanner;
 
+public class Conjunto {
+	static Scanner  input = new Scanner (System.in);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int num ;
+		boolean estaA;
+		boolean estaB;
+		boolean estaAmb;
+		boolean estaNin;
+		System.out.println("ingrese un numero ");
+		num = Integer.parseInt(input.nextLine());
+		
+			estaA= (num>-10 && num<10);
+			estaB = (num%2 !=0);
+			estaAmb = (estaA && estaB);
+			estaNin = !(estaA || estaB);
+			
+			System.out.println("Esta en A " + "			" + estaA);
+			System.out.println("Esta en B " + "			" +estaB);
+			System.out.println("Esta en ambos " +"			"+ estaAmb);
+			System.out.println("No esta en ninguno "+"		"+ estaNin);
+			
+			}
+		}
+	
 
-	}
 
-}
+	
